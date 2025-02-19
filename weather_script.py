11051653e22bce4bb97171d82ea48d87c50ede15
yijunnykim <163805134+yijunnykim@users.noreply.github.com>
2025-02-19 14:34:19 +0900

    Update weather_script.py
    OpenWeather API를 활용해
    서울 날씨 정보를 get방식으로 요청
    응답 결과 중 
    온도(temp), 습도(humidity), 설명(description : 맑음)을
    seoul_weather.csv에 저장

--- weather_script.py
+++ weather_script.py
@@ -1,3 +1,45 @@
 # openweather api로 요청을 해서 
 # 그 결과를 csv로 저장하는 py
 
+import requests
+import csv
+from datetime import datetime
+import os
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+CITY = "Seoul"
+URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+response = (requests.get(URL))
+data = response.json()
+print(data)
+
+# main -> temp
+# main -> humidity
+# weather -> description
+temp = data["main"]["temp"]
+humidity = data["main"]["humidity"]
+description = data["weather"][0]["description"]
+timezone = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(timezone)
+
+# 위의 4개의 데이터를 가지는 csv파일 생성
+csv_filename = "seoul_weather.csv"
+header = ["timezone", "temp", "humidity", "description"]
+
+# csv가 존재 -> True
+# csv가 존재 X -> False 
+file_exist = os.path.isfile(csv_filename)
+
+# mode = "a" 
+# -> if is not file -> create
+# if is file -> write
+with open(csv_filename, "a", newline="") as file:
+    writer = csv.writer(file)
+
+    # 파일이 존재하지 않는다( = 헤더가 없다) -> 헤더 추가 필요
+    if not file_exist:
+        writer.writerow(header)
+
+    writer.writerow([timezone, temp, humidity, description])
+
